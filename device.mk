d89ff382b41aa6792520b0d799b0a3d22bdde1e7
Vineel Sai <vineelsai26@gmail.com>
2020-11-14 09:28:35 +0000

    fix build errors

--- device.mk
+++ device.mk
@@ -305,7 +305,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
     init.safailnet.rc
 
 PRODUCT_PACKAGES += \
